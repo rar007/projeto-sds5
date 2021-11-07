2cb5b87ddb693398d482b6cc64af008ba0f7b5ae
Renan Rodrigues <renan_jacui@hotmail.com>
2021-11-07 10:00:59 +0000

    Routes

---
+++ frontend/src/pages/Dashboard/index.tsx
@@ -0,0 +1,33 @@
+import BarChat from "components/BarChat";
+import DataTable from "components/DataTable";
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+import DonutChart from 'components/DonutChart';
+
+function Dashboard() {
+  return (
+    <>
+      <NavBar />
+      <div className="container">
+        <h1 className="text-primary py-3">DashBoard de vendas</h1>
+        <div className="row px-3">
+          <div className="col-sm-6">
+            <h5 className="text-secundary text-center">Taxa de sucesso (%)</h5>
+            <BarChat />
+          </div>
+          <div className="col-sm-6">
+            <h5 className="text-secundary text-center">Todas as vendas</h5>
+            <DonutChart />
+          </div>
+        </div>
+        <div className="py-3">
+          <h2 className="text-primary">Todas vendas</h2>
+        </div>
+        <DataTable />
+      </div>
+      <Footer />
+    </>
+  );
+}
+
+export default Dashboard;
---
+++ frontend/src/pages/Home/index.tsx
@@ -0,0 +1,8 @@
+function Home() {
+    return (
+        <h1>Pagina home</h1>
+    );
+  }
+  
+  export default Home;
+  
